13ba7c52e5d8d717cf3b971fb7d2e9834968a1a9
ajiaron <aaronjiang2001@gmail.com>
2024-07-19 13:30:51 -0700

    more gtag config

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Head from 'next/head';
 import "../styles/globals.scss";
 import { GeistSans } from 'geist/font/sans';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={GeistSans.className}>
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-8J2XT06MFK" />
     </html>
   );
 }
